842933f8155e16a85816d2dbbf81179c4744b24b
ChronicCH <448333189@qq.com>
2020-11-01 16:25:05 +0800

    feat:Promise练习

---
+++ homework/test.js
@@ -0,0 +1,35 @@
+/**
+ * @param api 网络请求的地址
+ * @param callback 网络请求结束后处理的回调函数
+ * @return
+ * */
+function fetchWithCallback(api, callback) {
+    setTimeout(function () {
+        const response = 'yeah!'
+        callback(response);
+    }, 1000)
+}
+fetchWithCallback('',function (result) {
+    console.log(result);
+})
+/**
+ * @param api 网络请求的地址
+ * @return Promise 供后续逻辑的处理
+ * */
+function fetchPromisify(api) {
+    return new Promise(((resolve, reject) => {
+        const response = 'yeah!';
+        resolve(response);
+    }))
+}
+
+/**
+ * @param api 网络请求的地址
+ * @return Promise 供后续逻辑的处理
+ * */
+async function fetch(api) {
+    let a = await fetchPromisify(api);
+    console.log(a);
+}
+
+fetch('');
