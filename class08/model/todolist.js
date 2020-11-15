1c2c06d78a848f3ccb5c6601359c12e50e9a90d3
new Kuku() <kkukuli@tencent.com>
2020-11-15 17:05:23 +0800

    feat: more MVC code

--- class08/model/todolist.js
+++ class08/model/todolist.js
@@ -9,7 +9,7 @@ var Todolist = (function (){
             todolist.push(todo);
             return todo;
         },
-        query({id, title, content} = {}) {
+        query({id, title, content, done} = {}) {
             if (id)
                 return todolist.find(todo => todo.id === id);
             if (title && content)
@@ -20,8 +20,27 @@ var Todolist = (function (){
                 return todolist.filter(todo => todo.title.indexOf(content) !== -1)
             if (content)
                 return todolist.filter(todo => todo.content.indexOf(content) !== -1)
+            if (done)
+                return todolist.filter(todo => todo.done === done);
             return [].slice.call(todolist);
         },
+        update(id, {title, content, done}) {
+            console.log(id);
+            console.log(todolist);
+            const index = todolist.findIndex(todo => todo.id === id);
+            if (index === -1) {
+                return;
+            }
+            if (title) {
+                todolist[index].title = title;
+            }
+            if (content) {
+                todolist[index].content = content;
+            }
+            if (done !== undefined) {
+                todolist[index].done = done;
+            }
+        }
     }
 
     return Todolist;
