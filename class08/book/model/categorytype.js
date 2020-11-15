2d4ddd3bb1437782a12761926567ba35f00be5d6
dayao <541950428@qq.com>
2020-11-15 20:02:43 +0800

    fix：add findByName and 命名改驼峰

--- class08/book/model/categorytype.js
+++ class08/book/model/categorytype.js
@@ -1,17 +1,16 @@
 //category model
 
 // id 全局变量
-var CategoryTypeId= 0;
+var categoryTypeId= 0;
 
 // 自增：生成唯一 id的方法
 function getCategoryTypeId() {
-    return CategoryTypeId++;
+    return categoryTypeId++;
 }
 
 var CategoryType = (function () {
     function CategoryType({name}) {
-        // 有这个方法吗？？
-        this.id = CategoryTypeId(); // 类id
+        this.id = getCategoryTypeId(); // 类id
         this.name = name; // 类名
     }
     return CategoryType
