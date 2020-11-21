fbf059306c49cbadb05a04a2698a87b5c39bef0f
ChronicCH <448333189@qq.com>
2020-11-21 21:09:49 +0800

    feat: registry todolist

--- class08/registry/model/UserType.js
+++ class08/registry/model/UserType.js
@@ -2,11 +2,48 @@ var index = 0;
 function getId(){
     return index++;
 }
-// todo 完成缺少的UserTypeList
+
 var UserType = (function (){
-    function UserType(name){
+    function UserType({name}){
         this.name = name;
         this.id = getId();
     }
     return UserType;
+}())
+
+var UsertypeList = (function (){
+    var typelist = [];
+
+    function UsertypeList(){};
+
+    UsertypeList.prototype = {
+        add({name}){
+            var user = new UserType({name});
+            typelist.push(user);
+            return user;
+        },
+        remove: function (id){
+            return typelist.filter(type => {
+                return type.id !== Number(id);
+            })
+        },
+        query: function ({id} = {}){
+            if(typeof id !== "undefined"){
+                return typelist.find(type => {
+                    return type.id === Number(id);
+                })
+            }
+            return [].slice.call(typelist);
+        },
+        update: function (id,name){
+            const index = typelist.findIndex(type => type.id === id);
+            if(index === -1){
+                return;
+            }
+            if(name){
+                typelist[index].name = name;
+            }
+        }
+    }
+    return UsertypeList;
 }())
