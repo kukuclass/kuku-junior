625e84b5b82f17d7671855010366e371d3d02a4a
dayao <541950428@qq.com>
2020-11-26 17:21:51 +0800

    fix：book （补class8-MVC）

--- class08/book/model/categoryTypeList.js
+++ class08/book/model/categoryTypeList.js
@@ -1,41 +1,65 @@
+//category model
+
+// id 全局变量
+var categoryTypeId= 0;
+
+// 自增：生成唯一 id的方法
+function getCategoryTypeId() {
+    return categoryTypeId++;
+}
+
+var CategoryType = (function () {
+    function CategoryType({name}) {
+        this.id = getCategoryTypeId(); // 类id
+        this.name = name; // 类名
+    }
+
+    return CategoryType
+}())
+
+
 // todo 文件名全部修改为驼峰命名或下划线命名
 var CategoryTypeList = ( function() {
     //声明一个能装book的bookList
-    var categoryTypeList = []
+    var categoryTypes = []
     function CategoryTypelist(){}
     CategoryTypelist.prototype = {
         //add
-        add(name){
-            categoryTypeList.push( new CategoryType(name))
+        addByCategory({name}){
+            var category = new CategoryType({name})
+            categoryTypes.push( category)
+            return category
         },
         //delete
         delete(id){
-            return categoryTypeList.filter(item => item.id !== id)
+            return categoryTypes.filter(item => item.id !== id)
         },
         //find todo find by name
-        find(id, name){
-            if(id !== undefined){
-                return categoryTypeList.find(item => item.id === id);
-            }
-            if(name){
-                //模糊匹配
-                return categoryTypeList.filter(item =>{
-                    return item.name.indexOf(name) !== -1
+        find({id}){
+            if (typeof id !== "undefined") {
+                return categoryTypes.find(function (category) {
+                    return category.id === Number(id)
                 })
             }
+            // if(name !== undefined ){
+            //     //模糊匹配
+            //     return categoryTypes.filter(item =>{
+            //         return item.name.indexOf(name) !== -1
+            //     })
+            // }
         },
 
         //update
         update(id, name){
             //先根据id找到这条记录在bookList中对应的index
-            var index = categoryTypeList.findIndex(item => item.id === id)
+            var index = categoryTypes.findIndex(item => item.id === id)
             //如果index为-1，不存在，直接return
             if(index !== -1){
                 return
             }
             // 否则根据index修改对应的内容
             if(name){
-                categoryTypeList[index].name = name
+                categoryTypes[index].name = name
             }
 
         },
